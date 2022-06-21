58c7a05dbc97cffdc9729a9c332aa4049e8b8acf
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2022-06-21 23:17:26 +0200

    [dxvk] Introduce DxvkContextType

--- src/dxvk/dxvk_context.h
+++ src/dxvk/dxvk_context.h
@@ -22,7 +22,7 @@ namespace dxvk {
     constexpr static VkDeviceSize StagingBufferSize = 4ull << 20;
   public:
     
-    DxvkContext(const Rc<DxvkDevice>& device);
+    DxvkContext(const Rc<DxvkDevice>& device, DxvkContextType type);
     ~DxvkContext();
     
     /**
@@ -1071,6 +1071,7 @@ namespace dxvk {
   private:
     
     Rc<DxvkDevice>          m_device;
+    DxvkContextType         m_type;
     DxvkObjects*            m_common;
     
     Rc<DxvkCommandList>     m_cmd;
