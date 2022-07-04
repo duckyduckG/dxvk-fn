af0025ff881f3459ea85b5356470b79ddc9f889a
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2022-07-04 20:33:36 +0200

    [dxvk] Drop barrier before renderPassBindFramebuffer
    Not necessary since that function emits barriers anyway.

--- src/dxvk/dxvk_context.cpp
+++ src/dxvk/dxvk_context.cpp
@@ -3753,8 +3753,6 @@ namespace dxvk {
 
       m_flags.clr(DxvkContextFlag::GpRenderPassSuspended);
 
-      m_execBarriers.recordCommands(m_cmd);
-
       this->renderPassBindFramebuffer(
         m_state.om.framebufferInfo,
         m_state.om.renderPassOps,
