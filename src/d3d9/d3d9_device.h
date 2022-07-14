682101b48f5121ae0968cdbd2fa6536657dbb98c
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2022-07-14 16:15:59 +0200

    [d3d9] Rework UP buffer allocation

--- src/d3d9/d3d9_device.h
+++ src/d3d9/d3d9_device.h
@@ -975,7 +975,8 @@ namespace dxvk {
 
     void DetermineConstantLayouts(bool canSWVP);
 
-    template<bool UpBuffer>
+    D3D9BufferSlice AllocUPBuffer(VkDeviceSize size);
+
     D3D9BufferSlice AllocTempBuffer(VkDeviceSize size);
 
     bool ShouldRecord();
@@ -1169,7 +1170,10 @@ namespace dxvk {
     D3D9ConstantBuffer              m_psFixedFunction;
     D3D9ConstantBuffer              m_psShared;
 
-    D3D9BufferSlice                 m_upBuffer;
+    Rc<DxvkBuffer>                  m_upBuffer;
+    VkDeviceSize                    m_upBufferOffset  = 0ull;
+    void*                           m_upBufferMapPtr  = nullptr;
+
     D3D9BufferSlice                 m_managedUploadBuffer;
 
     D3D9Cursor                      m_cursor;
